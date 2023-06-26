573a832684fc123400e0ec05f88ecc3dd8df82ea
K-Sikora <krzysieksikora00123@gmail.com>
2023-06-26 14:36:14 +0200

    Add shadcn, style homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider>
-      <Component {...pageProps} />
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
     </ClerkProvider>
   );
 };
