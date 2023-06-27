d4f6f5e92a48c4ceaf2c3e136f4c0df9d6108ed0
K-Sikora <krzysieksikora00123@gmail.com>
2023-06-27 14:24:30 +0200

    Require auth for add page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
-  publicRoutes: ["/(.*)"],
+  // publicRoutes: ["/(.*)"],
+  publicRoutes: ["/", "/resources", "/api/(.*)"],
 });
 
 export const config = {
